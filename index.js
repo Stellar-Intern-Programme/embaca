9ba25d5ca46722f0cc2e98a1e2b147a511f3bdcd
Cosmin Backup <cosmin.durbaca2006@gmail.com>
2022-06-30 14:26:23 +0300

    click

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
+let weather
+fetch('./weather.json').then(res => res.json()).then(data => {
+    weather = data
+    renderWeather()
+})
+function renderWeather() {
+    let divP = document.querySelector(".time")
+    divP.innerHTML = ""
+    Object.keys(weather).forEach((item) => {
+        let p = document.createElement("p")
+        p.textContent = item
+        divP.appendChild(p)
+        p.addEventListener("click", () => {
+       
+            renderWeatherData(item)
+            const children = divP.children
+           for(let i=0; i<children.length; i++){
+               children[i].className = ""
+           }
+           
+            p.className = "active"
+        })
+    })
+
+}
+
+
+function renderWeatherData() {
+ 
+
+}
+
 let cityArray = JSON.parse(localStorage.getItem("city")) || []; //se salveaza in local storeage
 let cityList;
 let activeCity;
@@ -6,6 +38,7 @@ window.addEventListener("load", (event) => {
     cityList = document.getElementById("images");
     redrawCities();
     selectCity(cityArray[0]);
+    renderWeather()
 });
 
 function openModal() {
