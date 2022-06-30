b33d09707fa388a6f1db50d6fae6d1864b5b6d38
EmiTirnovan <tirnovan.emilian2004@gmail.com>
2022-06-30 14:37:29 +0300

    working on weather

--- index.js
+++ index.js
@@ -1,134 +1,158 @@
+let weather;
+fetch('./weather.json').then(res => { return res.json() }).then(data => {
+  weather = data;
+  renderWeather()
+  console.log(weather)
+});
+function renderWeather() {
+  Object.keys(weather).forEach((item) => {
+    console.log(item)
+  })
+}
 let cityArray = JSON.parse(localStorage.getItem("city")) || []; //se salveaza in local storeage
 let cityList;
 let activeCity;
 window.addEventListener("load", (event) => {
-    //functia asta se apeleaza dupa ce se incarca tot
-    cityList = document.getElementById("images");
-    redrawCities();
-    selectCity(cityArray[0]);
+  //functia asta se apeleaza dupa ce se incarca tot
+  cityList = document.getElementById("images");
+  redrawCities();
+  selectCity(cityArray[0]);
 });
 
 function openModal() {
-    let modal = document.getElementById("modal");
-    modal.style.display = "flex";
-    const inputPhoto = document.getElementById("photoURLinput");
-    inputPhoto.addEventListener("input", onInputChange); // pentru a schimba background-ul dupa ce inserez un URL
+  let modal = document.getElementById("modal");
+  modal.style.display = "flex";
+  const inputPhoto = document.getElementById("photoURLinput");
+  inputPhoto.addEventListener("input", onInputChange); // pentru a schimba background-ul dupa ce inserez un URL
 }
 
 function submitForm() {
-    const cityName = document.getElementById("searchInput");
-    const photo = document.getElementById("photoURLinput");
-    cityArray.push({ photo: photo.value, cityName: cityName.value });
-    localStorage.setItem("city", JSON.stringify(cityArray)); //adaug valorile in array
-    closeModal(); //inchid modalul
-    redrawCities(); //adaug un oras nou
-    cityName.value = "";
-    photo.value = "";
-    let background = document.getElementById("background_image");
-    background.style.background = "none";
-    imgContent.style.display = "flex";
+  const cityName = document.getElementById("searchInput");
+  const photo = document.getElementById("photoURLinput");
+  cityArray.push({ photo: photo.value, cityName: cityName.value });
+  localStorage.setItem("city", JSON.stringify(cityArray)); //adaug valorile in array
+  closeModal(); //inchid modalul
+  redrawCities(); //adaug un oras nou
+  cityName.value = "";
+  photo.value = "";
+  let background = document.getElementById("background_image");
+  background.style.background = "none";
+  imgContent.style.display = "flex";
 }
 
 function redrawCities() {
-    const cityList = document.getElementById("images");
-    cityList.removeChild = ".another_city"; // sterg ultimul copil (div), ca sa nu imi mai dubleze
-    const addCard = document.querySelector(".add");
-    cityList.innerHTML = "";
-    cityList.appendChild(addCard); //adaug un copil la add
-    cityArray.forEach(cityMap); // aici e for-ul pentru oameni smecheri, care parcurge array-ul prin / cu functia cityMap
+  const cityList = document.getElementById("images");
+  cityList.removeChild = ".another_city"; // sterg ultimul copil (div), ca sa nu imi mai dubleze
+  const addCard = document.querySelector(".add");
+  cityList.innerHTML = "";
+  cityList.appendChild(addCard); //adaug un copil la add
+  cityArray.forEach(cityMap); // aici e for-ul pentru oameni smecheri, care parcurge array-ul prin / cu functia cityMap
 }
 //functia cityMap pentru array-ul city, unde fac div-ul pentru oras cu imaginea si textul si ii dau clasa "another_city"
 function selectCity(city) {
-    const location = document.getElementById("location");
-    if (city && city.hasOwnProperty("cityName")) {
-        location.innerHTML = city.cityName;
-        activeCity = city;
-    }
+  const location = document.getElementById("location");
+  if (city && city.hasOwnProperty("cityName")) {
+    location.innerHTML = city.cityName;
+    activeCity = city;
+  }
 }
 
 function cityMap(city) {
-    const cityList = document.getElementById("images");
-    let cityParent = document.createElement("div");
-    cityParent.addEventListener("click", () => {
-        selectCity(city); //am functia pentru a selecta un oras
-        cityParent.className = "selected_city";
-    }); //fac un div
-    let img = document.createElement("img"); //ii adaug tag-ul de imagine
-    let text = document.createElement("p"); //adaug tagul de text
-    img.setAttribute("src", city.photo); //adaug atributul de src in tagul de la img cu imaginea salvata in array
-    text.innerHTML = city.cityName; // la fel si la text
-    cityParent.appendChild(img);
-    cityParent.appendChild(text);
-    cityParent.className = "another_city"; // ii dau clasa "another_city"
-    const addCard = document.querySelector(".add");
-    cityList.insertBefore(cityParent, addCard); //inserez orasul inaintea butonului de add
+  const cityList = document.getElementById("images");
+  let cityParent = document.createElement("div");
+  cityParent.addEventListener("click", () => {
+    selectCity(city); //am functia pentru a selecta un oras
+    cityParent.className = "selected_city";
+  }); //fac un div
+  let img = document.createElement("img"); //ii adaug tag-ul de imagine
+  let text = document.createElement("p"); //adaug tagul de text
+  img.setAttribute("src", city.photo); //adaug atributul de src in tagul de la img cu imaginea salvata in array
+  text.innerHTML = city.cityName; // la fel si la text
+  cityParent.appendChild(img);
+  cityParent.appendChild(text);
+  cityParent.className = "another_city"; // ii dau clasa "another_city"
+  const addCard = document.querySelector(".add");
+  cityList.insertBefore(cityParent, addCard); //inserez orasul inaintea butonului de add
 }
 
 function closeModal() {
-    let modal = document.getElementById("modal");
-    modal.style.display = "none";
+  let modal = document.getElementById("modal");
+  modal.style.display = "none";
 }
 
 function onInputChange(event) {
-    const inputPhoto = document.getElementById("photoURLinput");
-    let background = document.getElementById("background_image");
-    background.style.backgroundImage = `url("${event.target.value}")`; //modific background image cu valoarea la photoInput
-    background.style.backgroundSize = "cover";
-    background.style.backgroundRepeat = "none";
-    let imgContent = document.getElementById("imgContent");
-    imgContent.style.display = "none"; //fac sa dispara imaginea de inceput
-    console.log(event);
+  const inputPhoto = document.getElementById("photoURLinput");
+  let background = document.getElementById("background_image");
+  background.style.backgroundImage = `url("${event.target.value}")`; //modific background image cu valoarea la photoInput
+  background.style.backgroundSize = "cover";
+  background.style.backgroundRepeat = "none";
+  let imgContent = document.getElementById("imgContent");
+  imgContent.style.display = "none"; //fac sa dispara imaginea de inceput
+  console.log(event);
 }
 
 function deleteCity() {
-    cityArray = cityArray.filter(city => city.cityName !== activeCity.cityName)
-    console.log(cityArray, activeCity)
-    localStorage.setItem("city", JSON.stringify(cityArray))
-    redrawCities();
-    selectCity(cityArray[0])
-    cityList.children[0].className = "selected_city"
+  cityArray = cityArray.filter((city) => city.cityName !== activeCity.cityName);
+  console.log(cityArray, activeCity);
+  localStorage.setItem("city", JSON.stringify(cityArray));
+  redrawCities();
+  selectCity(cityArray[0]);
+  cityList.children[0].className = "selected_city";
 }
 
 function openRecomends() {
-    let recomends = document.getElementById("recomends");
-    recomends.style.display = "flex";
+  let recomends = document.getElementById("recomends");
+  recomends.style.display = "flex";
 }
 
 function selectedCityInput(element) {
-    const cityName = document.getElementById("menuSearchInput");
-    console.log(cityName);
-    cityName.value = element.innerText;
-    console.log(element.innerText);
-    removeRecommend();
-    search(cityName);
+  const cityName = document.getElementById("menuSearchInput");
+  console.log(cityName);
+  cityName.value = element.innerText;
+  console.log(element.innerText);
+  removeRecommend();
+  search(cityName);
 }
 
 function removeRecommend() {
-    let recomends = document.getElementById("recomends");
-    recomends.style.display = "none";
+  let recomends = document.getElementById("recomends");
+  recomends.style.display = "none";
 }
 
 function recommend(e) {
-    if (e.value === "") {
-        redrawCities();
-        removeRecommend();
-        return;
-    }
-    console.log(e.value);
-    let v = cityArray.filter((q) => q.cityName.toLowerCase().includes(e.value));
-    console.log(v);
-    let recomends = document.getElementById("recomends");
-    recomends.innerHTML = "";
-    v.forEach((w) => {
-        recomends.innerHTML += `<p onclick="selectedCityInput(this)">${w.cityName}</p>`;
-    });
+  if (e.value === "") {
+    redrawCities();
+    removeRecommend();
+    return;
+  }
+  console.log(e.value);
+  let v = cityArray.filter((q) => q.cityName.toLowerCase().includes(e.value));
+  console.log(v);
+  let recomends = document.getElementById("recomends");
+  recomends.innerHTML = "";
+  v.forEach((w) => {
+    recomends.innerHTML += `<p onclick="selectedCityInput(this)">${w.cityName}</p>`;
+  });
 }
 
 function search(e) {
-    let remain = cityArray.filter((f) => f.cityName.includes(e.value));
-    console.log(remain);
-    cityList.innerHTML = "";
-    cityList.innerHTML = `<div class="add" id="add" onclick="openModal()">
+  let remain = cityArray.filter((f) => f.cityName.includes(e.value));
+  console.log(remain);
+  cityList.innerHTML = "";
+  cityList.innerHTML = `<div class="add" id="add" onclick="openModal()">
     <div class="plus">+</div><a>Add city</a></div>`;
-    remain.forEach((e) => cityMap(e));
+  remain.forEach((e) => cityMap(e));
+}
+
+function renderWeatherContent() {
+  const leftCharts = document.getElementById("leftCharts");
+  const chart1 = document.getElementById("chart1")
+  chart1.style.display = "flex";
+  const daysCharts = document.getElementById("daysCHarts");
+  daysCharts.appendChild(p);
+  const p = createElement("p");
+  p.textContent = item
+  const umidity = document.getElementById("umidity");
+  const img = createElement("img");
+  img.setAttribute("src", "src/rain_drop.svg");
 }
